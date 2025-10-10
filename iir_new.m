b7b8649f4f92606ab3a5abfa7604f5e56264131b
Gengqi-Liu <151445599+Gengqi-Liu@users.noreply.github.com>
2025-10-10 12:46:06 +0200

    Update iir_new.m

--- iir_new.m
+++ iir_new.m
@@ -1,60 +1,54 @@
 %% ------------------------------------------------------------------------
-% Generate IIR_filters.mat compatible with hybrid FIR+IIR real-time script
+% FIR to IIR Conversion (Hybrid 5.1, 2 ears × 6 Tx)
+% Output: mIIR_B, mIIR_A as 3D arrays [2,6,nB/nA]
 % ------------------------------------------------------------------------
 clear; clc;
 
 %% 1. Load FIR Data
 load('filters/Room_Home_221025_5_1_HP_HD800_221025.mat'); % mIRInt: [samples, ears, speakers, angles]
-
 fs = 44100;                % Sampling frequency
-cut_time = 0.08;           % 80 ms cutoff for tail
-cut_idx = round(cut_time * fs);
+cut_time = 0.08;           % 80 ms
+cut_idx = round(cut_time * fs); % 对应样本点索引
 
-[num_samples, num_ears, num_speakers, num_angles] = size(mIRInt);
-
+num_ears = 2;
+num_tx   = 6;              % 5.1声道: 6 Tx
+angle_idx = 53;            % 固定角度
 %% 2. IIR Design Parameters
-B_order = 7500; % 分子阶数
-A_order = 7500; % 分母阶数
+B_order = 7500;
+A_order = 7500;
 
-%% 3. Preallocate mIIR struct array
-mIIR = cell(num_ears, num_speakers);
+%% 3. Preallocate storage as 3D arrays
+mIIR_B = zeros(num_ears, num_tx, B_order+1);
+mIIR_A = zeros(num_ears, num_tx, A_order+1);
 
-%% 4. Loop over ears and speakers
-for ear = 1:num_ears
-    for spk = 1:num_speakers
-        % 使用角度索引 53
-        angle_idx = 53;
+%% 4. Loop over ears and Tx
+for iCRx = 1:num_ears
+    for iCTx = 1:num_tx
+        % Get FIR
+        h_fir_full = squeeze(mIRInt(:, iCRx, iCTx, angle_idx));
         
-        % 完整 FIR
-        h_fir_full = squeeze(mIRInt(:, ear, spk, angle_idx));
+        % Split head and tail
+        h_head = h_fir_full(1:cut_idx);
+        h_tail = h_fir_full(cut_idx+1:end);
         
-        % ---------------------------
-        % 切断尾部
-        h_fir_head = h_fir_full(1:cut_idx);       % 保留前80ms
-        h_fir_tail = h_fir_full(cut_idx+1:end);   % 尾部拟合
-        
-        % ---------------------------
-        % FFT
-        N_fft = length(h_fir_tail);
-        H_tail = fft(h_fir_tail);
+        % FFT of tail
+        N_fft = length(h_tail);
+        H_tail = fft(h_tail);
         f_half = (0:floor(N_fft/2))*(fs/N_fft);
         H_half = H_tail(1:length(f_half));
+        omega = 2*pi*f_half/fs; % normalized frequency [0,pi]
+        W = ones(size(H_half));  % weights
         
-        omega = 2*pi*f_half/fs;
-        W = ones(size(H_half));
-        
-        % ---------------------------
-        % invfreqz 拟合 IIR
-        disp(['Designing IIR for Ear ', num2str(ear), ' Speaker ', num2str(spk)]);
+        % invfreqz least-squares IIR
+        disp(['Designing IIR for Rx ', num2str(iCRx), ', Tx ', num2str(iCTx)]);
         [B_iir, A_iir] = invfreqz(H_half, omega, B_order, A_order, W);
         
-        % ---------------------------
-        % 保存到 struct
-        mIIR{ear, spk} = struct('b', B_iir, 'a', A_iir);
+        % Store as 3D array
+        mIIR_B(iCRx, iCTx, 1:length(B_iir)) = B_iir;
+        mIIR_A(iCRx, iCTx, 1:length(A_iir)) = A_iir;
     end
 end
 
-%% 5. Save
-save('IIR_filters.mat', 'mIIR');
-
-disp('IIR_filters.mat generated successfully!');
+%% 5. Save as .mat for direct use
+save('IIR_filters.mat','mIIR_B','mIIR_A');
+disp('IIR_filters.mat saved (3D arrays, compatible with real-time script).');
